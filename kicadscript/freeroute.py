f57f8f2b28569ffe6163344003cff5fc0ecfc123
mafe72 <mafe72@users.noreply.github.com>
2019-01-29 15:06:28 -0600

    Script to Enable Freerouting in Kicad 5.x

---
+++ kicadscript/freeroute.py
@@ -0,0 +1,17 @@
+import pcbnew
+import os
+
+class Freeroute(pcbnew.ActionPlugin):
+    def defaults(self):
+        self.name = "Freeroute"
+        self.category = "Autorouting"
+        self.description = "Triggers Freerouting"
+
+    def Run(self):
+        # The entry function of the plugin that is executed on user action
+        board = pcbnew.GetBoard()
+        basename = os.path.splitext(board.GetFileName())
+        
+        os.system(pcbnew.GetKicadConfigPath()+'\\FreeRouting.jar -de \"'+basename[0]+'.dsn\"')
+
+Freeroute().register() # Instantiate and register to Pcbnew
